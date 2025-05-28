403d211df680f65da0ef997209773a5d04e13e6e
Najeb Johnson <najeb.johnson@oit.edu>
2025-05-28 01:03:47 -0700

    fix v6

--- index.js
+++ index.js
@@ -11,14 +11,14 @@ app.use(cors());
 app.use(express.json());
 
 // Serve static HTML files from your actual folder name: 'rod management'
-app.use(express.static(path.join(__dirname, 'rod management')));
+app.use(express.static(path.join(__dirname, 'rod-management')));
 
 // API routes
 app.use('/api', formRoutes);
 
 // Serve dashboard.html on root route
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'rod management', 'dashboard.html'));
+  res.sendFile(path.join(__dirname, 'rod-management', 'dashboard.html'));
 });
 
 // (Optional) If you ever add a 404 page named 404.html in that folder:
